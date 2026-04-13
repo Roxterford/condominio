9e36e7a08bf74b4cbff0988b4b51e52c6c993acc
sanaruca <sanaruca@gmail.com>
2026-04-13 00:50:23 -0400

    styles

---
+++ apps/panel/src/app/(panel)/layout.tsx
@@ -0,0 +1,65 @@
+import { ChartPie, House, LayoutGrid, Newspaper, Settings } from "lucide-react";
+import Link from "next/link";
+import styles from "./layout.module.css";
+
+export default function PanelLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex">
+      <Navbar />
+      <main className="w-full py-5 px-10">{children}</main>
+    </div>
+  );
+}
+
+function Navbar() {
+  return (
+    <nav className="border-r pt-10 px-5">
+      <ul className="grid gap-5 place-items-center">
+        <li>
+          <Link href="/" className={styles.navlink}>
+            <div className={styles["navlink__icon-content"]}>
+              <LayoutGrid className={styles.navlink__icon} />
+            </div>
+            Inicio
+          </Link>
+        </li>
+        <li>
+          <Link href="/villas" className={styles.navlink}>
+            <div className={styles["navlink__icon-content"]}>
+              <House className={styles.navlink__icon} />
+            </div>
+            Villas
+          </Link>
+        </li>
+        <li>
+          <Link href="/cuotas" className={styles.navlink}>
+            <div className={styles["navlink__icon-content"]}>
+              <Newspaper className={styles.navlink__icon} />
+            </div>
+            Cuotas
+          </Link>
+        </li>
+        <li>
+          <Link href="/reportes" className={styles.navlink}>
+            <div className={styles["navlink__icon-content"]}>
+              <ChartPie className={styles.navlink__icon} />
+            </div>
+            Reportes
+          </Link>
+        </li>
+        <li>
+          <Link href="/configuracion" className={styles.navlink}>
+            <div className={styles["navlink__icon-content"]}>
+              <Settings className={styles.navlink__icon} />
+            </div>
+            Configuración
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
