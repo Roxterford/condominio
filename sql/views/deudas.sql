cdb413cb0039c6954f5ea3733b3189b290dda25b
sanaruca <sanaruca@gmail.com>
2026-09-12 15:59:06 +0000

    fix: deuda saldada

--- sql/views/deudas.sql
+++ sql/views/deudas.sql
@@ -8,7 +8,7 @@ SELECT
   c.monto AS monto,
   c.monto - COALESCE(SUM(dp.destinado), 0) AS deuda,
   CASE
-    WHEN COALESCE(SUM(dp.destinado), 0) = c.monto THEN 'PAGADA'
+    WHEN COALESCE(SUM(dp.destinado), 0) = c.monto THEN 'SALDADA'
     WHEN COALESCE(SUM(dp.destinado), 0) = 0 THEN 'PENDIENTE'
     WHEN COALESCE(SUM(dp.destinado), 0) < c.monto THEN 'ABONADA'
   END AS estado,
